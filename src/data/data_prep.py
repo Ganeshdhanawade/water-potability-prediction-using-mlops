71c835758674f9efff3c8ee5e453affda9c406a3
Ganesh Dhanawade <dhanagan@nmvs.com>
2024-10-15 16:35:40 +0530

    run 1st data pipeline using dvc

--- src/data/data_prep.py
+++ src/data/data_prep.py
@@ -43,7 +43,7 @@ def main():
         train_processed_data = fill_missing_with_mean(train_data)
         test_processed_data = fill_missing_with_mean(test_data)
 
-        os.makedirs(processed_data_path)
+        os.makedirs(processed_data_path,exist_ok=True)
 
         save_data(train_processed_data, os.path.join(processed_data_path,"train_processed_mean.csv"))
         save_data(test_processed_data, os.path.join(processed_data_path,"test_processed_mean.csv"))
